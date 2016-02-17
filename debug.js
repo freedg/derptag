33bf1b56c2b9c14aadbdff1b560f99a75451b854
freedg <freedg@users.noreply.github.com>
2016-02-16 22:51:33 -0600

    3.0 outputs correctly when this file is pasted into console doesn't add character namespace to tags when copied via userscript

--- debug.js
+++ debug.js
@@ -21,7 +21,38 @@ function copyDerpibooruTags(){
 			}
 		});
 	}).then(function(value) {
-		tags = tags.replace("artist:", "creator:");
+		addNamespaces(tags);
+	});
+}
+
+function addNamespaces(tags) {
+	$.getJSON('https://raw.githubusercontent.com/freedg/derptag/master/array.json', function(array) {
+		var rating;
+		var j = 0;
+		while (array['rating'][j]){
+			rating = array['rating'][j]
+			for (i=0; i < tags.length; i++){
+				if (rating === tags[i]){
+					tags[i] = tags[i].replace(rating, 'rating:' + rating)					
+				} 
+			}
+			j++;
+		} 
+	});
+	$.getJSON('https://raw.githubusercontent.com/freedg/derptag/master/array.json', function(array) {
+		var rating;
+		var character
+		var j = 0;
+		while (array['character'][j]){
+			character = array['character'][j]
+			for (i=0; i < tags.length; i++){
+				if (tags[i] === character){
+					tags[i] = tags[i].replace(character, 'character:' + character)
+				} 
+			}
+			j++;
+		} console.log(tags);
 		copyTagsToClipboard(tags);
 	});
 }
+copyDerpibooruTags();
